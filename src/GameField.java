3d7016ea98299de0d23705a1fc58474f43db6ed9
Vladimir <70746959+immortalBS@users.noreply.github.com>
2020-10-25 18:09:13 +0300

    Создание основных методов
    Добавлен метод инициализации игры
    Описан метод движения
    Добавлен таймер
    Метод создания яблока
    Метод поедания яблока
    Метод отображения графики
    Метод проверки столкновения с саамим собой и бордюром
    Имплитиролвание срабатывания таймера

--- src/GameField.java
+++ src/GameField.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
 
-public class GameField extends JPanel {
+public class GameField extends JPanel implements ActionListener {
     //Создание игровых компонентов
     private final int SIZE = 320; //Размер игрового поля
     private final int DOT_SIZE = 16; //Размер ячейки
@@ -31,7 +34,35 @@ public class GameField extends JPanel {
         setBackground(Color.black);
         //Вызов метода который водгружает картинки
         loadImages();
+        initGame();
     }
+    //Метод инициализации игры:
+    public void initGame(){
+        //установка начального количествка точек (3)
+        dots = 3;
+        for (int i = 0; i < dots; i++) {
+            // Начальная x позиция
+            x[i] = 48 - i * DOT_SIZE;
+            // Начальная Y позиция
+            y[i] = 48;
+        }
+        // Создание таймера
+        // this необходима для того чтобы при
+        // каждом вызове таймера, использовался этот, в классе GameField
+        // для этого также необходимо создать специальный метод и имплимитировать специальный интерфейс
+        timer = new Timer(250,this);
+        // Запуск таймера
+        timer.start();
+        // Ввызов метода для создания яблока
+        createApple();
+    }
+
+    public void createApple() {
+        // Создание рандомной позиции
+        appleX = new Random().nextInt(20)*DOT_SIZE;
+        appleY = new Random().nextInt(20)*DOT_SIZE;
+    }
+
     //Метод ддля загрузки картинок
     public void loadImages() {
         //Для того чтобы загрузить картинки с изображением dot и apple:
@@ -44,5 +75,82 @@ public class GameField extends JPanel {
 
     }
 
+    // Не могу понять что это и почему, но оно отображает змею и яблоки
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (inGame);
+        g.drawImage(apple, appleX, appleY, this);
+        for (int i = 0; i <dots ; i ++) {
+            g.drawImage(dot, x[i], y[i], this);
+        }
+    }
 
+    public void move() {
+        //Все точки (не головы) перемещаются на предыдущие позиции
+        for (int i = dots; i > 0; i--){
+            x[i] = x[i-1];
+            y[i] = y[i-1];
+        }
+        // Для головы перемещение в ту сторону которая true (boolean):
+        if (left) {
+            x[0] -= DOT_SIZE;
+        }
+        if (right) {
+            x[0] += DOT_SIZE;
+        }
+        if (up) {
+            y[0] += DOT_SIZE;
+        }
+        if (down) {
+            y[0] -=  DOT_SIZE;
+        }
+    }
+    // Если координаты дота равны координатам яблока, то срабатывает метод createApple
+    public void checkApple() {
+        if (x[0] == appleX && y[0] == appleY) {
+            dots++;
+            createApple();
+        }
+
+    }
+
+    public void checkCollisions() {
+        // Проверка столкновения самим с собой
+        for (int i = dots; i > 0; i--){
+            if(i>4 && x[0] == x[i] && y[0] == y[i]){
+                inGame = false;
+            }
+        }
+        // Проверка столкновения с бордюрами
+        if (x[0] > SIZE) {
+            inGame = false;
+        }
+        if (x[0] < 0) {
+            inGame = false;
+        }
+        if (y[0] > SIZE) {
+            inGame = false;
+        }
+        if (y[0] < 0) {
+            inGame = false;
+        }
+    }
+
+    // Метод вызываемый таймером каждый раз когда он тикает (250)
+    // Его мы и имплимитировали
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // Если мы в игре то...
+        if (inGame) {
+            // Если мы в игре змейка должна двигаться. необходимо создать метод move
+            checkApple();
+            // Метод который проверяет столкновение с бордюрами и с самим собой
+            checkCollisions();
+            move();
+
+        }
+        // Метод вызывает pain companent для перерисовки (стандартная функция винды)
+        repaint();
+    }
 }
