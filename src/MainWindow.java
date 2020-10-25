fb64beff295ec8611e0c5813d3daa60095900417
Vladimir <70746959+immortalBS@users.noreply.github.com>
2020-10-25 14:08:00 +0300

    Описание основного окна игры.
    Создание и разметка формы с помощью Swing

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,2 +1,22 @@
-public class MainWindow {
+import javax.swing.*;
+//Здесь будут описаны основные настройки окна
+//Так же будет добавленно игровое поле (GameField)
+
+public class MainWindow extends JFrame{
+    //Создание конструктора
+    public MainWindow() {
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //Размер окна:
+        setSize(320, 345);
+        setLocation(400, 400);
+        // Добавление экземпляра класса на окно:
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Создание экземпляра. Место начала работы программы
+        MainWindow mw = new MainWindow();
+    }
 }
